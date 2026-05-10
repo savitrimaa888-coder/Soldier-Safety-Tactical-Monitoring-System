67324a43ac871f6690dd7ddc52e8b21b30c0b4a9
Radhika Jakkannavar <savitrimaa888@gmail.com>
2026-05-10 16:14:56 +0530

    Add initial build configuration for Android app

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.tacticalfieldcommunicationsystemwithiot"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.tacticalfieldcommunicationsystemwithiot"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+    // 🔥 FIXED LIBRARIES
+    implementation(libs.volley)
+    implementation(libs.play.services.location)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
